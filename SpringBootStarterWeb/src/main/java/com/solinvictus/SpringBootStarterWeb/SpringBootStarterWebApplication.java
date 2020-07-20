3efa2994faff4d27396726152162180cc7ce8983
aayushsahu <ayush297@gmail.com>
2020-07-20 21:55:54 +0530

    First Commit of demo Application

---
+++ SpringBootStarterWeb/src/main/java/com/solinvictus/SpringBootStarterWeb/SpringBootStarterWebApplication.java
@@ -0,0 +1,13 @@
+package com.solinvictus.SpringBootStarterWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterWebApplication.class, args);
+	}
+
+}
